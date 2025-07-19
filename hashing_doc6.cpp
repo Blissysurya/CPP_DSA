0b1a8395d13c865e1ba242069baf671b0d8c4900
Suryansh Dixit <suryanshdixit4924.dpsjkp@gmail.com>
2025-07-20 00:15:07 +0530

    updates

---
+++ hashing_doc6.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+istream& operator>> ( istream& in,vector<int>& arr){
+    for(int i=0;i<n;i++){
+        in>>arr[i];
+    }
+    return in;
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    int k;
+    cin>>k;
+
+
+    vector<int> vec(n);
+    
+    for(int i=0;i<n;i++){
+
+    }
+}
