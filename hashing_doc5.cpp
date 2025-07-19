49ed55b711ab96c3dedc60eb6b8d045de9768378
Suryansh Dixit <suryanshdixit4924.dpsjkp@gmail.com>
2025-07-19 23:55:30 +0530

    completed doc5

---
+++ hashing_doc5.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    int k;
+    cin>>k;
+
+    vector<int> vec(n);
+
+    for(int i=0;i<n;i++){
+        cin>>vec[i];
+    }
+
+    map<int,int> mp;
+
+    vector<int> pref(n);
+    pref[0]=vec[0];
+
+    for(int i=1;i<n;i++){
+        pref[i]=pref[i-1]+vec[i];
+    }
+
+    map<int,int> mp;
+
+    mp[0]=0;
+    int cnt=0;
+
+    for(int i=0;i<n;i++){
+        if(mp[pref[i]-k]){
+            cnt += mp[pref[i]-k] 
+        }
+        mp[pref[i]]++;
+    }
+
+    cout<<cnt<<endl;
+    
+}
