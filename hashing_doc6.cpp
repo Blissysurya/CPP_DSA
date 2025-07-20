9acd6186ca518afb1357d85149dd0f564ab2f56c
Blissysurya <150290655+Blissysurya@users.noreply.github.com>
2025-07-20 17:24:02 +0530

    updates

--- hashing_doc6.cpp
+++ hashing_doc6.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 istream& operator>> ( istream& in,vector<int>& arr){
-    for(int i=0;i<n;i++){
+    for(int i=0;i<(int)arr.size();i++){
         in>>arr[i];
     }
     return in;
@@ -20,7 +20,35 @@ int main(){
 
     vector<int> vec(n);
     
-    for(int i=0;i<n;i++){
+    cin>>vec;
 
+    map<int,int> mp;
+
+    mp[0]=0;
+
+    vector<int> pref(n);
+    pref[0]=vec[0];
+
+    for(int i=1;i<n;i++){
+        pref[i]=pref[i-1]+vec[i];
     }
+
+    int ans=0;
+
+    // code for max Subarray with sum==k
+
+    for(int i=0;i<n;i++){
+        if(mp[pref[i]-k]){
+            ans = max(ans,i-mp[pref[i]-k]);
+        }
+
+        int x=mp[pref[i]-k];
+        
+        if(mp.find(x)==mp.end()){
+            mp[x]=i;
+        }
+    }
+
+    cout<<ans<<endl;
+
 }
